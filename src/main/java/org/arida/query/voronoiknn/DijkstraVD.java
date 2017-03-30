6f738361a9af5c3e4d1ede6c289d162b660ff02c
Gustavo Coutinho <gustavolgcr@gmail.com>
2017-03-30 12:28:05 -0300

    Adding an adjacentPolygons structure.

--- src/main/java/org/arida/query/voronoiknn/DijkstraVD.java
+++ src/main/java/org/arida/query/voronoiknn/DijkstraVD.java
@@ -21,76 +21,86 @@ import it.unimi.dsi.fastutil.longs.Long2IntMap;
 
 public class DijkstraVD {
 
-	//External references
+	// External references
 	private Graph graph;
 	private Set<Long> globalSettleNodes = new HashSet<>();
 	private Queue<DistanceEntry> globalUnsettleNodes = new PriorityQueue<>();
 	private Map<Long, HashSet<Long>> polygonBorderPoints = new HashMap<>();
+	private Map<Long, Long> nodeToPoIMap = new HashMap<>();
+	private Map<Long, Set<Long>> adjacentPolygons = new HashMap<>();
 	private Node source;
-	
+
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
-	
+
 	private int wasRemoved = -1;
 
-	//Internal attributes
+	// Internal attributes
 	private Queue<DistanceEntry> queue = new PriorityQueue<>();
 	private Map<Long, Integer> wasTraversed = new HashMap<>();
 	private Map<Long, Integer> wasTraversedPoI = new HashMap<>();
 	private Map<Long, RouteEntry> parents = new HashMap<>();
 	private DistanceEntry removed = null;
 
-	public DijkstraVD(Graph graph, Node source, Set<Long> globalSettleNodes, Queue<DistanceEntry> globalUnsettleNodes, Map<Long, HashSet<Long>> polygonBorderPoints) {
-		
+	public DijkstraVD(Graph graph, Node source, Set<Long> globalSettleNodes, Queue<DistanceEntry> globalUnsettleNodes,
+			Map<Long, HashSet<Long>> polygonBorderPoints, Map<Long, Long> nodeToPoIMap, Map<Long, Set<Long>> adjacentPolygons) {
+
 		this.graph = graph;
 		this.source = source;
 		this.globalSettleNodes = globalSettleNodes;
 		this.globalUnsettleNodes = globalUnsettleNodes;
 		this.polygonBorderPoints = polygonBorderPoints;
-		
+		this.nodeToPoIMap = nodeToPoIMap;
+		this.adjacentPolygons = adjacentPolygons;
+
 		init(source, queue);
 
 	}
 
 	public void init(Node source, Queue<DistanceEntry> queue) {
-		
+
 		int sid = convertToInt(source.getId());
 		queue.offer(new DistanceEntry(sid, 0, -1));
-	
+
 	}
 
 	public void iterate() {
 
 		removed = queue.poll();
 		logger.debug("Node being analyzed: {}", removed.getId());
-		
-		if(globalSettleNodes.contains(removed.getId())) {
 
-			HashSet<Long> newSet = polygonBorderPoints.get(source.getId());
-			newSet.add(removed.getParent());
-			polygonBorderPoints.replace(source.getId(), newSet);
+		if (globalSettleNodes.contains(removed.getId())) {
+
+			Set<Long> newAdjacentSet = adjacentPolygons.get(source.getId());
+			newAdjacentSet.add(nodeToPoIMap.get(removed.getId()));
+			adjacentPolygons.replace(source.getId(), newAdjacentSet);
 			
+			HashSet<Long> newBorderSet = polygonBorderPoints.get(source.getId());
+			newBorderSet.add(removed.getParent());
+			polygonBorderPoints.replace(source.getId(), newBorderSet);
+
 			return;
-			
+
 		} else {
-			
+
 			wasTraversed.put(removed.getId(), wasRemoved);
 			globalSettleNodes.add(removed.getId());
-			
+			nodeToPoIMap.put(removed.getId(), source.getId());
+
 		}
-		
+
 		expandVertex(removed, queue, parents, wasTraversed, wasTraversedPoI);
 
 	}
-	
-	public void expandVertex(DistanceEntry removed, Queue<DistanceEntry> queue,
-			Map<Long, RouteEntry> parents, Map<Long, Integer> wasTraversed,
-			Map<Long, Integer> wasTraversedPoI) {
+
+	public void expandVertex(DistanceEntry removed, Queue<DistanceEntry> queue, Map<Long, RouteEntry> parents,
+			Map<Long, Integer> wasTraversed, Map<Long, Integer> wasTraversedPoI) {
 
 		Long2IntMap neighbors = graph.accessNeighborhood(graph.getNode(removed.getId()));
 
 		for (long vid : neighbors.keySet()) {
 
-			DistanceEntry newEntry = new DistanceEntry(vid, removed.getDistance() + neighbors.get(vid), removed.getId());
+			DistanceEntry newEntry = new DistanceEntry(vid, removed.getDistance() + neighbors.get(vid),
+					removed.getId());
 
 			Edge edge = null;
 			int distance = -1;
@@ -98,23 +108,23 @@ public class DijkstraVD {
 			if (!wasTraversed.containsKey(vid)) {
 
 				queue.offer(newEntry);
-				//colocar esse nó na queue global
-				
-				
-				//Verificar se ja existe esse nó no globalSettleNodes. Se sim, colocar a distancia
-				//como border distance
-				//colocar esse nó na globalPriorityQueue
-				
-				
+				// colocar esse nó na queue global
+
+				// Verificar se ja existe esse nó no globalSettleNodes. Se sim,
+				// colocar a distancia
+				// como border distance
+				// colocar esse nó na globalPriorityQueue
+
 				wasTraversed.put(newEntry.getId(), newEntry.getDistance());
 
 				distance = neighbors.get(vid);
 				edge = getEdge(removed.getId(), vid, distance);
 
 				parents.put(vid, new RouteEntry(removed.getId(), distance, edge.getId(), edge.getLabel()));
-				
-				//TODO Create a similar class, just like the DistanceEntry.
-				//Esse é uma entrada especial. Está apenas mapeando a atual distancia para o PoI correpondente
+
+				// TODO Create a similar class, just like the DistanceEntry.
+				// Esse é uma entrada especial. Está apenas mapeando a atual
+				// distancia para o PoI correpondente
 				globalUnsettleNodes.add(new DistanceEntry(source.getId(), newEntry.getDistance(), vid));
 
 			} else {
@@ -126,35 +136,34 @@ public class DijkstraVD {
 					if (cost > distance) {
 						queue.remove(newEntry);
 						queue.offer(newEntry);
-						
+
 						wasTraversed.remove(newEntry.getId());
-						
-						//colocar esse nó na globalPriorityQueue
-						//TODO
-//						globalPriorityQueue.add(new DistanceEntry(source.getId(), distance, -1l));
-						
+
+						// colocar esse nó na globalPriorityQueue
+						// TODO
+						// globalPriorityQueue.add(new
+						// DistanceEntry(source.getId(), distance, -1l));
+
 						wasTraversed.put(newEntry.getId(), newEntry.getDistance());
 
-						//TODO Double check if this is working
+						// TODO Double check if this is working
 						newEntry.setId(source.getId());
 						newEntry.setParent(vid);
 						newEntry.setDistance(distance);
 						globalUnsettleNodes.remove(newEntry);
 						globalUnsettleNodes.add(new DistanceEntry(source.getId(), distance, vid));
-						
+
 						parents.remove(vid);
 						distance = neighbors.get(vid);
 						edge = getEdge(removed.getId(), vid, distance);
 						parents.put(vid, new RouteEntry(removed.getId(), distance, edge.getId(), edge.getLabel()));
-						
-						
 
 					}
 				}
 			}
 		}
 	}
-	
+
 	private Edge getEdge(long fromNodeId, long toNodeId, int distance) {
 		Edge edge = null;
 		for (Long outEdge : graph.getOutEdges(fromNodeId)) {
--- src/main/java/org/arida/query/voronoiknn/VoronoiDiagram.java
+++ src/main/java/org/arida/query/voronoiknn/VoronoiDiagram.java
@@ -17,110 +17,109 @@ public class VoronoiDiagram {
 
 	private Graph g;
 	private Map<Long, Node> pois = new HashMap<Long, Node>();
-	
+
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
-	
+
 	/**
-	 * Map that will store an instance of a modified Dijkstra Algorithm
-	 * to each Point of Interest (PoI).
+	 * Map that will store an instance of a modified Dijkstra Algorithm to each
+	 * Point of Interest (PoI).
 	 */
-	private Map<Long, DijkstraVD> dijkstraHash = new HashMap<Long, DijkstraVD>();
-	
+	private Map<Long, DijkstraVD> dijkstraHash = new HashMap<>();
+
+	private Map<Long, Long> nodeToPoIMap = new HashMap<>();
+
 	/**
-	 * border2BorderDistance will store the distance between all pairs of border points
-	 * in the same cell. A HashMap that maps the first border point (NodeID-Long) to a HashMap
-	 * that maps the second border point (NodeID-Long) to the distance (distance - Integer) 
-	 * between them.
+	 * border2BorderDistance will store the distance between all pairs of border
+	 * points in the same cell. A HashMap that maps the first border point
+	 * (NodeID-Long) to a HashMap that maps the second border point
+	 * (NodeID-Long) to the distance (distance - Integer) between them.
 	 */
 	private Map<Long, HashMap<Long, Integer>> border2BorderDistance;
-	
+
 	/**
-	 * border2PoIDistance will store the distance between all border points and their respective
-	 * Point of Interest (PoI). A HashMap that maps the border point (NodeID-Long) to a HashMap
-	 * that maps the Point of Interest (NodeID-Long) to the distance (distance - Integer) 
-	 * between them. 
+	 * border2PoIDistance will store the distance between all border points and
+	 * their respective Point of Interest (PoI). A HashMap that maps the border
+	 * point (NodeID-Long) to a HashMap that maps the Point of Interest
+	 * (NodeID-Long) to the distance (distance - Integer) between them.
 	 */
 	private Map<Long, HashMap<Long, Integer>> border2PoIDistance;
-	
+
 	/**
-	 * polygonBorderPoints will store all borderPoints that belongs to a certain Voronoi Cell (polygon).
-	 * A HashMap that maps the Point of Interest (NodeID-Long) of the cell to a set of all 
-	 * border points (Set of NodeID's)
+	 * polygonBorderPoints will store all borderPoints that belongs to a certain
+	 * Voronoi Cell (polygon). A HashMap that maps the Point of Interest
+	 * (NodeID-Long) of the cell to a set of all border points (Set of NodeID's)
 	 */
 	private Map<Long, HashSet<Long>> polygonBorderPoints = new HashMap<>();
-	
+
 	/**
-	 * adjacentPolygons will store all adjacent polygons of a certain Voronoi Cell (polygon).
-	 * A HashMap that maps the Point of Interest (NodeID-Long) of the cell to a set of all 
-	 * adjacent polygons Points of Interest (Set of NodeID's)
+	 * adjacentPolygons will store all adjacent polygons of a certain Voronoi
+	 * Cell (polygon). A HashMap that maps the Point of Interest (NodeID-Long)
+	 * of the cell to a set of all adjacent polygons Points of Interest (Set of
+	 * NodeID's)
 	 */
-	private Map<Long, Set<Long>> adjacentPolygons;
-	
-	//TODO Verificar para que serve essa variável
-	private Map<Long, Long> voronoiCell;
-	
+	private Map<Long, Set<Long>> adjacentPolygons = new HashMap<>();
+
 	private Set<Long> globalSettleNodes = new HashSet<Long>();
-	
-	//TODO criar um SET com nós que já são settle
-	
 	private Queue<DistanceEntry> globalUnsettleNodes = new PriorityQueue<>();
-	
+
 	public VoronoiDiagram(Graph g) {
-		
+
 		this.g = g;
-		
-		for(Node poi : g.getPOIsNodes()) {
+
+		for (Node poi : g.getPOIsNodes()) {
 			DistanceEntry distancePoI = new DistanceEntry(poi.getId(), 0, -1);
 			globalUnsettleNodes.add(distancePoI);
-			
+
 			pois.put(poi.getId(), poi);
-			
-//			globalSettleNodes.add(poi.getId());
+
 		}
-		
+
 	}
-	
+
 	/**
-	 * This method will run a slightly modified Dijkstra algorithm for each Point of Interest.
-	 * There's a simple scheduler in this method: the next PoI that will have it's respective
-	 * Dijkstra Algorithm runned is the one with the smaller unsettleNode distance.
+	 * This method will run a slightly modified Dijkstra algorithm for each
+	 * Point of Interest. There's a simple scheduler in this method: the next
+	 * PoI that will have it's respective Dijkstra Algorithm runned is the one
+	 * with the smaller unsettleNode distance.
 	 */
 	public void createDiagram() {
-		
-		//Initialize a Hash with an instance of Dijkstra Algorithm
-		//for each Point of Interest
-		for(Node poi : g.getPOIsNodes()) {
-			DijkstraVD dj = new DijkstraVD(g, poi, globalSettleNodes, globalUnsettleNodes, polygonBorderPoints);
+
+		// Initialize a Hash with an instance of Dijkstra Algorithm
+		// for each Point of Interest
+		for (Node poi : g.getPOIsNodes()) {
+			DijkstraVD dj = new DijkstraVD(g, poi, globalSettleNodes, globalUnsettleNodes, polygonBorderPoints,
+					nodeToPoIMap, adjacentPolygons);
 			dijkstraHash.put(poi.getId(), dj);
 			polygonBorderPoints.put(poi.getId(), new HashSet<Long>());
-			
+			adjacentPolygons.put(poi.getId(), new HashSet<Long>());
+
 		}
-		
-		//Corresponds to one iteration of vertex expansion, picking
-		//always the PoI with lowest node in the unsettle queue
-		while(!globalUnsettleNodes.isEmpty()) {
-			
+
+		// Corresponds to one iteration of vertex expansion, picking
+		// always the PoI with lowest node in the unsettle queue
+		while (!globalUnsettleNodes.isEmpty()) {
+
 			Long currentPoIID = pois.get(globalUnsettleNodes.poll().getId()).getId();
-			
+
 			logger.debug("PoI being iterated: {}", currentPoIID);
 			dijkstraHash.get(currentPoIID).iterate();
 
 		}
-		
+
 		System.out.println("Test");
-	
+
 	}
-	
+
 	public void createCells() {
-		
+
 	}
-	
+
 	private void calculateB2BDistance() {
-		
+
 	}
-	
+
 	private void calculateP2BDistance() {
-		
+
 	}
-	
+
 }
